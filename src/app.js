1c5d5c87e4781fdfba989bf6e704b3b173356f44
Swapnil Bhavsar <swapnil@chitranu.com>
2020-06-03 17:58:44 +0530

    ✨ Added example

--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@
 //We've got some basic info about Karen's home
 //Debug the type of data provided
 //Return the types concatenated in a single variable
+function moreAboutHome(str, num, bool) {
+  var strType = typeof str;
+  var numType = typeof num;
+  var boolType = typeof bool;
 
+  return strType + numType + boolType;
+}
 //Progression 2:
 //Check if the data given is of the right type
 //parents = String, noOfSiblings = Number, isNuclearFamily = Boolean
